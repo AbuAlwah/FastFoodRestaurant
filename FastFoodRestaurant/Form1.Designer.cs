855b78aaa9850d0fecceb2f80eeddde76c234c47
Ali Al-Thaqbani <abu7alwah@gmail.com>
2026-07-07 09:42:52 +0300

    Set up side menus for fast food, drinks and desserts, and include a checkBox with a pictureBox.

--- FastFoodRestaurant/Form1.Designer.cs
+++ FastFoodRestaurant/Form1.Designer.cs
@@ -29,10 +29,52 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            panel2 = new Panel();
-            panel3 = new Panel();
             label1 = new Label();
+            panel2 = new Panel();
+            checkBox6 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            label2 = new Label();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            checkBox7 = new CheckBox();
+            label3 = new Label();
+            panel5 = new Panel();
+            checkBox8 = new CheckBox();
+            checkBox9 = new CheckBox();
+            checkBox10 = new CheckBox();
+            checkBox11 = new CheckBox();
+            checkBox12 = new CheckBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -42,41 +84,313 @@
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1518, 136);
+            panel1.Size = new Size(1467, 136);
             panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(554, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(430, 46);
+            label1.TabIndex = 0;
+            label1.Text = "FAST FOOD RESTAURANT";
             // 
             // panel2
             // 
+            panel2.Controls.Add(pictureBox6);
+            panel2.Controls.Add(pictureBox5);
+            panel2.Controls.Add(pictureBox4);
+            panel2.Controls.Add(pictureBox3);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(checkBox6);
+            panel2.Controls.Add(checkBox5);
+            panel2.Controls.Add(checkBox4);
+            panel2.Controls.Add(checkBox3);
+            panel2.Controls.Add(checkBox2);
+            panel2.Controls.Add(checkBox1);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(panel4);
             panel2.Dock = DockStyle.Left;
             panel2.Location = new Point(0, 136);
             panel2.Name = "panel2";
             panel2.Size = new Size(346, 778);
             panel2.TabIndex = 1;
             // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(46, 653);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(18, 17);
+            checkBox6.TabIndex = 9;
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(46, 547);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(18, 17);
+            checkBox5.TabIndex = 8;
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(46, 433);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(18, 17);
+            checkBox4.TabIndex = 7;
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(46, 312);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(18, 17);
+            checkBox3.TabIndex = 6;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(46, 202);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(18, 17);
+            checkBox2.TabIndex = 5;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(46, 93);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(18, 17);
+            checkBox1.TabIndex = 4;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(114, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 38);
+            label2.TabIndex = 3;
+            label2.Text = "Fast Meal";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Yellow;
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(0, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(40, 778);
+            panel4.TabIndex = 0;
+            // 
             // panel3
             // 
+            panel3.Controls.Add(pictureBox12);
+            panel3.Controls.Add(pictureBox11);
+            panel3.Controls.Add(pictureBox10);
+            panel3.Controls.Add(pictureBox9);
+            panel3.Controls.Add(pictureBox8);
+            panel3.Controls.Add(pictureBox7);
+            panel3.Controls.Add(checkBox12);
+            panel3.Controls.Add(checkBox11);
+            panel3.Controls.Add(checkBox10);
+            panel3.Controls.Add(checkBox9);
+            panel3.Controls.Add(checkBox8);
+            panel3.Controls.Add(checkBox7);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(panel5);
             panel3.Dock = DockStyle.Right;
-            panel3.Location = new Point(1170, 136);
+            panel3.Location = new Point(1119, 136);
             panel3.Name = "panel3";
             panel3.Size = new Size(348, 778);
             panel3.TabIndex = 2;
             // 
-            // label1
+            // checkBox7
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(640, 59);
-            label1.Name = "label1";
-            label1.Size = new Size(288, 31);
-            label1.TabIndex = 0;
-            label1.Text = "FAST FOOD RESTAURANT";
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(18, 93);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(18, 17);
+            checkBox7.TabIndex = 10;
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(18, 12);
+            label3.Name = "label3";
+            label3.Size = new Size(270, 38);
+            label3.TabIndex = 4;
+            label3.Text = "Drinks And Dessert";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Yellow;
+            panel5.Dock = DockStyle.Right;
+            panel5.Location = new Point(304, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(44, 778);
+            panel5.TabIndex = 0;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(18, 202);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(18, 17);
+            checkBox8.TabIndex = 6;
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(18, 321);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(18, 17);
+            checkBox9.TabIndex = 6;
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            checkBox10.AutoSize = true;
+            checkBox10.Location = new Point(18, 433);
+            checkBox10.Name = "checkBox10";
+            checkBox10.Size = new Size(18, 17);
+            checkBox10.TabIndex = 6;
+            checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            checkBox11.AutoSize = true;
+            checkBox11.Location = new Point(18, 556);
+            checkBox11.Name = "checkBox11";
+            checkBox11.Size = new Size(18, 17);
+            checkBox11.TabIndex = 6;
+            checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // checkBox12
+            // 
+            checkBox12.AutoSize = true;
+            checkBox12.Location = new Point(18, 653);
+            checkBox12.Name = "checkBox12";
+            checkBox12.Size = new Size(18, 17);
+            checkBox12.TabIndex = 6;
+            checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(90, 93);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 62);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(90, 202);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(125, 62);
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(90, 321);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(125, 62);
+            pictureBox3.TabIndex = 12;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(90, 433);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(125, 62);
+            pictureBox4.TabIndex = 13;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(90, 547);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(125, 62);
+            pictureBox5.TabIndex = 14;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(90, 653);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(125, 62);
+            pictureBox6.TabIndex = 15;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(67, 93);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(125, 62);
+            pictureBox7.TabIndex = 11;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(67, 202);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(125, 62);
+            pictureBox8.TabIndex = 12;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Location = new Point(67, 321);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(125, 62);
+            pictureBox9.TabIndex = 13;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Location = new Point(67, 433);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(125, 62);
+            pictureBox10.TabIndex = 14;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Location = new Point(67, 547);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(125, 62);
+            pictureBox11.TabIndex = 15;
+            pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Location = new Point(67, 653);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(125, 62);
+            pictureBox12.TabIndex = 16;
+            pictureBox12.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1518, 914);
+            ClientSize = new Size(1467, 914);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
@@ -85,6 +399,22 @@
             StartPosition = FormStartPosition.CenterScreen;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
             ResumeLayout(false);
         }
 
@@ -94,5 +424,33 @@
         private Panel panel2;
         private Label label1;
         private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Label label2;
+        private Label label3;
+        private CheckBox checkBox6;
+        private CheckBox checkBox5;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox7;
+        private PictureBox pictureBox1;
+        private CheckBox checkBox12;
+        private CheckBox checkBox11;
+        private CheckBox checkBox10;
+        private CheckBox checkBox9;
+        private CheckBox checkBox8;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
     }
 }
