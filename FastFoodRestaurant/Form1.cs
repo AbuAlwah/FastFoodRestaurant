22ec796bf0796bbbe7d12d9377ad6f2b157e0ae0
Ali Al-Thaqbani <abu7alwah@gmail.com>
2026-07-08 09:21:37 +0300

    Feat: update BUY button to display clean itemized receipt with subtotal calculation per item.

--- FastFoodRestaurant/Form1.cs
+++ FastFoodRestaurant/Form1.cs
@@ -198,7 +198,66 @@ namespace FastFoodRestaurant
             discountPrice.Text = "$" + discount.ToString("0.0");
             NetTotalPrice.Text = "$" + netTotal.ToString("0.0");
 
+        }
 
+        private void buyBtn_Click(object sender, EventArgs e)
+        {
+            if (NetTotalPrice.Text == "$/..")
+            {
+                MessageBox.Show("Please to calculate the total, you most first press the PRICE button before the BUY button!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            displayRichTextBox.Clear();
+
+            if (friesNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Fries\t\t{friesNum.Value}\t$1.0\t${friesNum.Value * 1.00m}\n");
+            }
+            if (burgerNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Burger\t\t{burgerNum.Value}\t$4.0\t${burgerNum.Value * 4.00m}\n");
+            }
+            if (saladNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Salad\t\t{saladNum.Value}\t$2.0\t${saladNum.Value * 2.00m}\n");
+            }
+            if (sandwichNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Sandwich\t\t{sandwichNum.Value}\t$3.0\t${sandwichNum.Value * 3.00m}\n");
+            }
+            if (chickenNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Chicken\t\t{chickenNum.Value}\t$5.0\t${chickenNum.Value * 5.00m}\n");
+            }
+            if (hotdogNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Hot Dog\t\t{hotdogNum.Value}\t$2.0\t${hotdogNum.Value * 2.00m}\n");
+            }
+            if (teaNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Tea\t\t{teaNum.Value}\t$0.3\t${teaNum.Value * 0.30m}\n");
+            }
+            if (sodaNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Soda\t\t{sodaNum.Value}\t$0.5\t${sodaNum.Value * 0.50m}\n");
+            }
+            if (waterNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Water\t\t{waterNum.Value}\t$0.2\t${waterNum.Value * 0.20m}\n");
+            }
+            if (chocolateNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Chocolate\t\t{chocolateNum.Value}\t$1.0\t${chocolateNum.Value * 1.00m}\n");
+            }
+            if (pancakesNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Pancakes\t\t{pancakesNum.Value}\t$3.0\t${pancakesNum.Value * 3.00m}\n");
+            }
+            if (cookiesNum.Value > 0)
+            {
+                displayRichTextBox.AppendText($"Cookies\t\t{cookiesNum.Value}\t$2.0\t${cookiesNum.Value * 2.00m}\n");
+            }
         }
     }
 }
